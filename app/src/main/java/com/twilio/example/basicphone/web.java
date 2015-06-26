f4c86f000c4cdf2aa8ad72d7d56e096edcbbf347
ad.langenkamp@gmail.com <Integra.01>
2015-06-26 10:08:12 +0200

    java web include

---
+++ app/src/main/java/com/twilio/example/basicphone/web.java
@@ -0,0 +1,11 @@
+package com.twilio.example.basicphone;
+
+/**
+ * Created by admin on 26/06/15.
+ */
+public class web {
+    mainActivity mainAct;
+    public web (mainActivity act) {
+        this.mainAct = act;
+    }
+}
